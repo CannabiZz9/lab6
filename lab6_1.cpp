abd10e726cd54570669fca7cc7f694c1e81fa5b4
CannabiZz9 <hydrahuracan1@gmail.com>
2022-12-13 15:13:31 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
+    int num;
+    int counteve = 0, countodd = 0;
+
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    while(num != 0)
+    {
+        if (num %2 == 0)
+        {
+            counteve++;
+        }
+        else
+        {
+            countodd++;
+        }
+        cout << "Enter an integer: ";
+        cin >> num;
+    }
+    cout << "#Even numbers = " << counteve <<"\n";
+    cout << "#Odd numbers = " << countodd <<"\n";
+
+
     return 0;
 }
